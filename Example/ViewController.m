267f0cdc3b28e9cf52505bba6b71b9d741ba3735
Rukmini K R <rukminikr@Rukminis-Mac-mini.local>
2013-01-17 12:24:51 +0530

    changes added

--- Example/ViewController.m
+++ Example/ViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+
+#define METERS_PER_MILE 1609.344
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+//@synthesize mapview;
 
 - (void)viewDidLoad
 {
@@ -24,6 +28,18 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (void)viewWillAppear:(BOOL)animated {
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 9.9667;
+    zoomLocation.longitude= 76.2167;
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // 3
+    [_mapView setRegion:viewRegion animated:YES];
 }
 
 @end
